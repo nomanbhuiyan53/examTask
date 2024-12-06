ce5df46e2ca27f94706aabddc3b6a26852172311
nomanbhuiyan53 <noman.bhuyan@gmail.com>
2024-12-06 22:53:51 +0600

    fixed

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -6,12 +6,11 @@
 @endpush
 @section('content')
     <div class="row g-10">
-        <div class="col-xl-12 col-lg-12">
+        <div class="col-xl-12 col-lg-12 p-4">
             <div class="panel mb-10">
                 <div class="panel-heading d-flex justify-content-between">
                     <span>Dashboard</span>
-                    <button class="btn btn-primary addBtn" data-bs-toggle="modal" data-bs-target="#addQuotationDetailsModal">Add Quotation Details</button>
-                </div>
+               </div>
                 <h4>Test Project</h4>
             </div>
         </div>
